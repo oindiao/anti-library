68704a8726b331f4d7e8db7f55918acd347c505d
Rafael Fernandes Pereira <oindiao@outlook.com>
2021-09-18 19:01:15 -0300

    adicionando API client

---
+++ client/src/main/java/com/github/oindiao/antilibrary/api/client/ClientApplication.java
@@ -0,0 +1,13 @@
+package com.github.oindiao.antilibrary.api.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+
+}
